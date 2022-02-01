ba01dc6d431e85e6be21ab96f8e6844f1869d5b9
Tuki1077 <70412089+Tuki1077@users.noreply.github.com>
2022-02-01 14:09:35 -0600

    Update main.py

--- Practica 2/main.py
+++ Practica 2/main.py
@@ -5,37 +5,49 @@ creditos = []
 debitos = []
 
 def total_creditos():
+    #Encontramos la suma total de los creditos usando "sum"
     print ("Sus creditos son: ")
     print (creditos)
     suma = sum(creditos)
     print ("La suma total de creditos es: ", suma)
 
 def total_debitos ():
+    #Encontramos la suma total de debitos usando "sum"
     print ("Sus debitos son:")
     print(debitos)
     suma_debitos = sum(debitos)
     print ("La suma total de debitos es: ", suma_debitos)
 
 def saldo():
+    #Saldo total es debitos-creditos
     suma_debitos = sum (debitos)
     suma_creditos = sum (creditos)    
     saldo = suma_debitos - suma_creditos
     print("Su saldo actual es: ", saldo)
 
 def avrg_debitos():
+    #Promedio es igual a la suma de los debitos / la cantidad de datos que se encuentran en el array
     promedio = sum (debitos) / len (debitos)
     print ("El promedio de debitos es", promedio)
 
 
 def debito_mayor():
+    #Primera hacemos sort el array de mas chico a mas grande
     debitos.sort()
     largest_element = debitos[0]
-
+    #Recorremos el array para encontrar el dato mas grande del array
     for i in range (0, len(debitos)):
         if (debitos[i] > largest_element):
             largest_element = debitos [i]
     
     print ("El debito mas grande es: ", largest_element)
+
+def eliminar_credito():
+    print ("Sus creditos actuales son:", creditos, "\n")
+    eliminar = int(input ("Indique segun la posicion, cual desea eliminar \n"))
+    creditos.pop(eliminar)
+    print ("Sus nuevos creditos son: ", creditos)
+
 
 
 
@@ -58,6 +70,7 @@ def main ():
             debitos.append(val_debitos)
 
     while (True):
+        print ("------------------------------")
         opcion = int (input ('Ingrese una opcion: \n 1. Total de Creditos \n 2. Total de Debitos \n 3. Saldo \n 4. Promedio de debitos \n 5. Mostrar Debito mas grande \n 6. Eliminar creditos \n 7. Exit \n'))
         print ("------------------------------")
 
